00a02032bd68393129b0e0b038107ba4c352217d
Superkorlas <superkorlas@gmail.com>
2020-02-28 14:53:03 +0100

    Project created.

---
+++ Práctica1/Sesion2/Sesion2_C++/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: super
+ *
+ * Created on 27 de febrero de 2020, 17:17
+ */
+
+#include "cstdlib"
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
